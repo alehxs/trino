770ac45e85df90ede00cd5b68412ec2c35aac22b
Alex <86182397+alehxs@users.noreply.github.com>
2026-01-28 21:17:52 -0600

    Initial Commit

---
+++ Trino/TrinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrinoApp.swift
+//  Trino
+//
+//  Created by Alex on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TrinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
