3e7c69a8e3aabd646f63bb27230bea09fb6d9719
Alex <86182397+alehxs@users.noreply.github.com>
2026-02-18 15:44:20 -0600

    chore: remove UserSettings SwiftData model in favor of AppStorage

--- Trino/TrinoApp.swift
+++ Trino/TrinoApp.swift
@@ -6,7 +6,7 @@ struct TrinoApp: App {
     var body: some Scene {
         WindowGroup {
             DashboardView()
-                .modelContainer(for: [TrinoTask.self, DailyLog.self, UserSettings.self, PendingSwap.self])
+                .modelContainer(for: [TrinoTask.self, DailyLog.self, PendingSwap.self])
         }
     }
 }
